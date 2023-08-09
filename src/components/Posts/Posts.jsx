cb85106503758142c3894b7f57ad9378a1aaf95b
dekkumo <houper8@gmail.com>
2023-08-09 12:55:17 +0300

    remove it

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import { useGetPostsQuery } from '../../store/PostsApi'
-import { app, db } from '../../firebase'
-import { collection, addDoc } from 'firebase/firestore'
 
 const Posts = () => {
-
-  // const {data} = useGetPostsQuery()
-  // console.log(data)
 
   return (
     <div className="_container">
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './slices/userSlice'
-import { postsApi } from "./PostsApi";
 import themeReducer from './slices/themeSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     theme: themeReducer,
-    // [postsApi.reducerPath]: postsApi.reducer
   },
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(postsApi.middleware)
 })
